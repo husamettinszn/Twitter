08b963d05c8e0258a3b144629f3b8d65a49952dd
husamettinszn <74509502+husamettinszn@users.noreply.github.com>
2022-01-25 10:40:38 +0100

    Update twitter.java

--- src/test/java/twitter.java
+++ src/test/java/twitter.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class twitter {
-
+// Hallo
     public static void main(String[] args) {
 
         WebDriverManager.chromedriver().setup();
